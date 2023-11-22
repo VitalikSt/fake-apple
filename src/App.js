4168ab01bbd39407d3ce213a3d68c7571da6eb54
Vitalik <vitalik.sts@gmail.com>
2023-11-22 19:24:01 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Component from './components/Component';
+import './App.scss';
+
+const App = () => {
+
+  return (
+		<>
+			<div className="container">
+				<Component />
+			</div>
+		</>
+	)
+}
+
+export default App;
---
+++ src/components/Component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Component = () => {
+	
+  return (
+		<>
+			<h1>Hello wold!</h1>
+		</>
+	)
+}
+
+export default Component;
